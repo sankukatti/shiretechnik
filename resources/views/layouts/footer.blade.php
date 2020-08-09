a3e61b5b079c6c8750228400ba0b116c251e87ea
shanky <snkukatti@gmail.com>
2020-08-09 22:01:34 +0530

    Updating About us page - Leadership to Team and social links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,23 +1,23 @@
 <section class="footer-section background-blue">
 	<div class="container">
 		<div class="row">
-			
+
 			<div class="col-sm-12">
 
 
 					<!-- <strong class="">Meet us</strong> -->
 					<div class="social-images-block">
 						<div class="creator">
-							Design & Developed by 
+							Design & Developed by
 							<a href="http://sandebtech.com/" target="_blank">
 								<img src="/images/footer/sandebtech.png">
 							</a>
 						</div>
 						<!-- <a href=""><img src="/images/socialIcons/facebook.png"></a> -->
-						<a href="" target="_blank"><img src="/images/socialIcons/google.png"></a>
-						<a href="" target="_blank"><img src="/images/socialIcons/linkedin.png"></a>
+						<!-- <a href="" target="_blank"><img src="/images/socialIcons/google.png"></a> -->
+						<a href="https://www.linkedin.com/company/shiretechnik-solutions-pvt-ltd" target="_blank"><img src="/images/socialIcons/linkedin.png"></a>
 						<!-- <a href=""><img src="/images/socialIcons/twitter.png"></a> -->
-						<a href="https://www.youtube.com/channel/UCKwD3x3tNsOECh5c24Wzb6g" target="_blank"><img src="/images/socialIcons/youtube.png"></a>
+						<a href="https://www.youtube.com/channel/UCt-MTZ1xOQM-osOhmjelGYA" target="_blank"><img src="/images/socialIcons/youtube.png"></a>
 					</div>
 			</div>
 
@@ -26,7 +26,7 @@
                     Copyright © 2017 Shiretechnik Solutions Pvt. Ltd.
 				</p>
 			</div>
-			
+
 		</div>
 	</div>
 </section>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,24 +12,24 @@
 					</p>
 
 					<p class="block text justify">
-						Shiretechnik is an engineering design analysis firm uniquely positioned to provide clients with superior results. Our experienced personnel, abundant resources, and state-of-the-art engineering analysis tools, labs and equipment with focus on clients' demands help us to achieve a unique place in the dynamic market that exists today. 
+						Shiretechnik is an engineering design analysis firm uniquely positioned to provide clients with superior results. Our experienced personnel, abundant resources, and state-of-the-art engineering analysis tools, labs and equipment with focus on clients' demands help us to achieve a unique place in the dynamic market that exists today.
 					</p>
-					
+
 
 					<p class="block text justify">
 						Since its Inception, Shiretechnik has established a reputation with excellent services to its customers, providing high-quality and cost-effective solutions. Each member of the team is customer-driven and brings a combination of design, analysis and test skills to resolve the issues faced by our customers. Most of our team members have 10 or more years of experience in solving thermal CFD problems in product development or research environment.
-					</p>				
+					</p>
 
 					<p class="title-text bold blue title-spacing">
-						
-					</p>				
+
+					</p>
 
 				</div>
 			</div>
-			
+
 
 			<p class="title-text bold blue title-spacing">
-				Leadership
+				Team
 			</p>
 
 			<hr/>
@@ -44,7 +44,7 @@
 					<p class="bold blue" >
 						Shankar S - Director Engineering
 					</p>
-				</div>	
+				</div>
 				<div class="col-sm-4" data-toggle="modal" data-target="#sandeep">
 					<img class="team-image" src="images/about/sandeep.png"/>
 					<p class="bold blue" >
@@ -62,13 +62,13 @@
 					<p class="bold blue">
 						Moumita- Design Consultant
 					</p>
-				</div>	 -->		
+				</div>	 -->
 			</div>
-			
+
 			<div class="row">
 				<div class="col-sm-12">
 					<p class="title-text bold blue title-spacing block">
-						Advisory Board Member 
+						Advisory Board Member
 					</p>
 				</div>
 			</div>
@@ -85,7 +85,7 @@
 					<p class="bold blue" >
 						Sarang Shidore
 					</p>
-				</div>	
+				</div>
 			</div>
 		</div>
 	</section>
@@ -154,11 +154,11 @@
 				</div>
 				<div class="modal-body">
 					<p class="block justify">
-						Shankar has seventeen years of professional experience on hardware product engineering and program management worked with the leading development teams in private sectors. He holds a Master’s degree in thermal engineering from Indian Institute of Technology (IIT), Chennai and two Bachelor degrees in mechanical engineering from Institute of Engineers, Kolkata and in mathematics from Madras University. He was in core development team for developing Thermal products FloTHERM, FloTHERM PACK. He keeps proven track records of managing complex functional hardware projects, and delivered the world-class products in Telecom, Defence, Aerospace and Consumer Applications. He received several honours &amp; awards in his professional career and notably winner of prestigious annual award for "Values Personification" in Leadership category from Aricent for the successful project management in the year 2013. He has deep expertise in CFD simulation tools with a special focus on electronics cooling analysis,and also authored the papers on thermal modelling in CFD environments in <a href="http://ieeexplore.ieee.org/document/4147290/">IEEE conferences.</a> 
+						Shankar has seventeen years of professional experience on hardware product engineering and program management worked with the leading development teams in private sectors. He holds a Master’s degree in thermal engineering from Indian Institute of Technology (IIT), Chennai and two Bachelor degrees in mechanical engineering from Institute of Engineers, Kolkata and in mathematics from Madras University. He was in core development team for developing Thermal products FloTHERM, FloTHERM PACK. He keeps proven track records of managing complex functional hardware projects, and delivered the world-class products in Telecom, Defence, Aerospace and Consumer Applications. He received several honours &amp; awards in his professional career and notably winner of prestigious annual award for "Values Personification" in Leadership category from Aricent for the successful project management in the year 2013. He has deep expertise in CFD simulation tools with a special focus on electronics cooling analysis,and also authored the papers on thermal modelling in CFD environments in <a href="http://ieeexplore.ieee.org/document/4147290/">IEEE conferences.</a>
 					</p>
 
 					<p class="block justify">
-						Certified Reliability Professional with Outstanding Grade from Centre for Reliability (CFR) – STQC Directorate, Ministry of Communication and Information Technology, Government of India. He has sound technical knowledge in Reliability Engineering. 
+						Certified Reliability Professional with Outstanding Grade from Centre for Reliability (CFR) – STQC Directorate, Ministry of Communication and Information Technology, Government of India. He has sound technical knowledge in Reliability Engineering.
 					</p>
 				</div>
 				<div class="modal-footer">
@@ -180,7 +180,7 @@
 				</div>
 				<div class="modal-body">
 					<p class="block justify">
-						Sandeep has more than twelve years of professional experience in CAE Software development domain and hardware product engineering in private sectors. He was the part of FloTHERM development team and also has worked for Dassault Systems, ESI etc. He holds Master’s and Bachelor’s degrees in Mechanical Engineering from Indian Institute of Technology (IIT), Kharagpur. He has deep expertise in CFD simulation tools. He is also co-founder of  SANDEBTech Pvt.Ltd. 
+						Sandeep has more than twelve years of professional experience in CAE Software development domain and hardware product engineering in private sectors. He was the part of FloTHERM development team and also has worked for Dassault Systems, ESI etc. He holds Master’s and Bachelor’s degrees in Mechanical Engineering from Indian Institute of Technology (IIT), Kharagpur. He has deep expertise in CFD simulation tools. He is also co-founder of  SANDEBTech Pvt.Ltd.
 					</p>
 					<p class="block justify">
 						Track Record - Dassault Systems, ESI, Flomerics India Pvt Ltd sportskeeda.com, policymantra.com.
@@ -227,7 +227,7 @@
 				</div>
 				<div class="modal-body">
 					<p class="block justify">
-						Moumita has eight years of professional experience in product development in automotive and petroleum industry. She holds a Master’s degree in Chemical Engineering from Indian Institute of Technology (IIT), Chennai. She has deep expertise in CFD simulation tools, worked in thermal modeling of electrochemical energy storage devices, computational fluid flow and heat analysis in Evaporator & Distillation Columns using COMSOL software. Her other areas of expertise are - Heat Transfer Analysis of Heat Exchangers, Optimization of natural and forced convection in storage tanks, MATLAB and Simulink modeling of battery and Solar PV modules. 
+						Moumita has eight years of professional experience in product development in automotive and petroleum industry. She holds a Master’s degree in Chemical Engineering from Indian Institute of Technology (IIT), Chennai. She has deep expertise in CFD simulation tools, worked in thermal modeling of electrochemical energy storage devices, computational fluid flow and heat analysis in Evaporator & Distillation Columns using COMSOL software. Her other areas of expertise are - Heat Transfer Analysis of Heat Exchangers, Optimization of natural and forced convection in storage tanks, MATLAB and Simulink modeling of battery and Solar PV modules.
 					</p>
 				</div>
 				<div class="modal-footer">
